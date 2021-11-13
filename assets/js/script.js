c2d300212725d7046d7b1a8b0ba037da86233386
brandon.sauer <brantheman8@comcast.net>
2021-11-13 09:44:09 -0800

    added border radius to buttons/inputs, realligned code, removed <a> tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-
-
 var options = {
-  method: 'GET',
-  url: 'https://yfapi.net/v6/finance/quote',
-  params: {region: 'us', lang:'en', symbols: 'AAPL,BTC-USD' },
+  method: "GET",
+  url: "https://yfapi.net/v6/finance/quote",
+  params: { region: "us", lang: "en", symbols: "AAPL,BTC-USD" },
   headers: {
-    'x-api-key': 'YvOamXCJ3f9vTVoV8zbHk3xtPqkf6K8x2aR22hXt'
-  }
+    "x-api-key": "YvOamXCJ3f9vTVoV8zbHk3xtPqkf6K8x2aR22hXt",
+  },
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-   
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -1,29 +1,32 @@
-$(document).ready(function() {
-    $("#search-button").on("click", function() {
-        var searchValue = $("#search-value").val();
+$(document).ready(function () {
+  $("#search-button").on("click", function () {
+    var searchValue = $("#search-value").val();
 
-        $("#search-value").val("");
+    $("#search-value").val("");
 
-        searchStock(searchValue);
-    });
-
+    searchStock(searchValue);
+  });
 });
 
 function searchStock(searchValue) {
-    $.ajax({
-        type: "GET",
-        url: "yfapi.net/v6/finance/quote?region=US&lang=en&symbols=%22%20%22" + searchValue + "XqHD0VzaV3InJ7ayecPd7DPJPwE6GoK7VDiJhmH1",
-        datatype: "json",
-        success: function(data) {
-            if (history.indexOf(searchValue) === -1) {
-                history.pushState(searchValue);
-                window.localStorage.setItem("history", JSON.stringify(history));
+  $.ajax({
+    type: "GET",
+    url:
+      "yfapi.net/v6/finance/quote?region=US&lang=en&symbols=%22%20%22" +
+      searchValue +
+      "XqHD0VzaV3InJ7ayecPd7DPJPwE6GoK7VDiJhmH1",
+    datatype: "json",
+    success: function (data) {
+      if (history.indexOf(searchValue) === -1) {
+        history.pushState(searchValue);
+        window.localStorage.setItem("history", JSON.stringify(history));
 
-                makeRow(searchValue);
-            }
-            var title = $("<h3>").addClass("card-title").text(data.name + "(" + new Number().toLocaleDateString() +")");
-            var card = $("<div>").addClass("Card");
-            
-        }
-    });
+        makeRow(searchValue);
+      }
+      var title = $("<h3>")
+        .addClass("card-title")
+        .text(data.name + "(" + new Number().toLocaleDateString() + ")");
+      var card = $("<div>").addClass("Card");
+    },
+  });
 }
