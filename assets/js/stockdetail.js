226e00505e67bd2343733ac5cc075cd668458ec1
nadiaalamgir21 <87517265+nadiaalamgir21@users.noreply.github.com>
2021-11-16 11:51:59 -0800

    css modified in stockdeail page.

--- assets/js/stockdetail.js
+++ assets/js/stockdetail.js
@@ -69,20 +69,14 @@ function printStockDetails(data) {
     <div class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="text-center mb-20">
-        <h1
-          class="
-            sm:text-3xl
-            text-2xl
-            font-medium
-            text-center
-            title-font
-            text-gray-900
-            mb-4
-          "
-        >
-          ${data.longName}
+     <h1 class="long-symbol-name">
+          ${data.longName} 
+          <p class="symbol-name">${data.symbol}</p>
         </h1>
-        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+        
+        
+
+        <p class="current-price">
           Market Price $${data.regularMarketPrice}
         </p>
       </div>
