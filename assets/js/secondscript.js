f1c8e8bf3c7d6336bbbd949b92927ba748e3e02d
thasingher <thasingher@gmail.com>
2021-11-16 17:53:00 -0800

    re-add scriptjs

--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -2,17 +2,10 @@ let apiKey = 'yRbGmb9wfwaWYv2NqP91C1NtIeTe2JUo8LxsQ9wk'
 
 $(document).ready(getValue)
 
-function getValue() {
-  let loc = window.location
-  let result = getUrlVars()
-  let stockName = result["stock"]
-  console.log("stock", stockName)
-  fetchStockDetails(stockName)
-}
 var options = {
   method: 'GET',
   url: 'https://yfapi.net/v1/finance/trending',
-  params: {region: "us", lang: "en", query: stockName },
+  params: {region: "us", lang: "en" },
   headers: {
     'x-api-key': 'yRbGmb9wfwaWYv2NqP91C1NtIeTe2JUo8LxsQ9wk'
   }
@@ -21,24 +14,10 @@ var options = {
 axios.request(options).then(function (response) {
 	console.log(response.data);
 }).catch(function (error) {
-	console.error(error);
 });
 
 function printStockDetails(data) {
-$(".stock-details").html(
-  `
-  <div class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="text-center mb-20">
-   <h1 class="long-symbol-name">
-        ${data.longName} 
-        <p class="symbol-name">${data.symbol}</p>
-    </h1>
-  </div>
-</div>
-  `
-)
-}
+  $(".stock-trending").html(
 
 // Read a page's GET URL variables and return them as an associative array.
 function getUrlVars() {
@@ -53,4 +32,5 @@ for (var i = 0; i < hashes.length; i++) {
   vars[hash[0]] = hash[1]
 }
 return vars
+});
 }
