7e4d839f797b1b4a31a3e62cd67908761e582b42
Eric Furukawa <eric.furukawa@gmail.com>
2021-11-20 18:04:45 -0800

    added function to store search input into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,29 @@ function initRun() {
   $("#search-button").on("click", function () {
     let val = $("#search-value").val()
     window.location = "stock-detail.html?stock=" + val
+    storeCompany(val);
   })
+};
+
+// add company name to companyArray
+var storeCompany = function (city) {
+  companyArray.push(city);
+  companyStorage();
+};
+
+// creating variables and array for storage
+var namesInStorage = localStorage.getItem("company-names");
+var companyArray;
+if (namesInStorage === null) {
+  companyArray = [];
+} else {
+  companyArray = JSON.parse(namesInStorage);
+};
+
+// set array to localStorage
+var companyStorage = function() {
+  localStorage.setItem("company-names", JSON.stringify(companyArray));
 }
+
 
 $(document).ready(initRun())
